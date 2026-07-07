fca3f21c70b58d7f7791fb0cee37b5f285b2e18d
Rafiul901 <rafiuldipu2480@gmail.com>
2026-07-07 21:19:19 +0600

    set up the auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import routes from './routes/index'
 const app = express();
 
 app.use(cors());
@@ -9,5 +9,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server is running");
 });
+app.use('/api',routes)
 
 export default app;
